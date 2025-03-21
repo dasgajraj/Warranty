51b11ddc6c7504f8951682e0501a9068cc1e349b
nishu-02 <0264nishu@gmail.com>
2025-03-22 03:55:22 +0530

    constraint

--- backend/slips/models.py
+++ backend/slips/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Slip(models.Model):
     product_name = models.CharField(max_length=255)
-    user_uid = models.CharField(max_length=255, unique=True)
+    user_uid = models.CharField(max_length=255)
     owner_wallet = models.CharField(max_length=255, null=True, blank=True)  # Add this field # Store the current owner's wallet address
     ipfs_hash = models.CharField(max_length=100, null=True, blank=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
